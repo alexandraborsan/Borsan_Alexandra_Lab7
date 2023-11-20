417fac4fad3fc18339483cbb0542f447185e3ac9
Alexandra Borsan <alexandra.borsan@gmail.com>
2023-11-20 20:13:29 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Borsan_Alexandra_Lab7
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
